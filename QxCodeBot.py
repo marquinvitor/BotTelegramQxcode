be24481e87df95c1045ecf1699bbe10a1eecfd7e
marcos vitor <marcosvitorsouzafreire@gmail.com>
2025-06-28 21:22:08 -0300

    bot funcional de avisos pro telegram

---
+++ QxCodeBot.py
@@ -0,0 +1,45 @@
+import telebot
+import schedule
+import threading
+import json
+
+
+def CarregarConfig():
+    with open('Bot/tokens.json', 'r') as f:
+        return json.load(f)
+    
+config = CarregarConfig()
+botToken = config['BOT_TOKEN']
+chat_id = config['CHAT_ID']
+
+bot = telebot.TeleBot(botToken)
+
+
+def enviarLembrete():
+    textoLembrete = "Não esqueçam de responder o diário de turmas!!!"
+    bot.send_message(chat_id, textoLembrete)
+    print("mensagem enviada com sucesso")
+
+
+horarioAgendado = "19:00" 
+schedule.every().monday.at(horarioAgendado).do(enviarLembrete)
+schedule.every().tuesday.at(horarioAgendado).do(enviarLembrete)
+schedule.every().wednesday.at(horarioAgendado).do(enviarLembrete)
+schedule.every().thursday.at(horarioAgendado).do(enviarLembrete)
+schedule.every().friday.at(horarioAgendado).do(enviarLembrete)
+schedule.every().saturday.at(horarioAgendado).do(enviarLembrete)
+
+def rodarAgendador():
+    while True:
+        schedule.run_pending()
+        
+
+
+if __name__ == "__main__":
+    print("iniciando bot")
+
+    threadAgendador = threading.Thread(target=rodarAgendador)
+    threadAgendador.daemon = True
+    threadAgendador.start()
+    bot.infinity_polling()
+
